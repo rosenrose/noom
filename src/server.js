ab26e61929547b456cbeec4b3261fdab9061e6ed
rosenrose <crazytempler@gmail.com>
2022-04-09 15:38:41 +0900

    path

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 import http from "http";
 import SocketIO from "socket.io";
 import express from "express";
+import path from "path";
 
 const app = express();
 
 app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname + "/public"));
+app.set("views", path.join(__dirname, "views"));
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
